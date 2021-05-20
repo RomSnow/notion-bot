ec22de466110e44e14822bc960a69665072459fd
Xaspy <20kolpakov01@gmail.com>
2021-05-20 13:02:00 +0500

    update logic to manage rooms and categories

--- source/src/main/java/tg/state/Transition.java
+++ source/src/main/java/tg/state/Transition.java
@@ -11,15 +11,22 @@ public class Transition {
         trans.put(new Pair<>(Condition.BEGIN, Alphabet.START), new Pair<>(Condition.HALL, Answer.Intro));
 
         trans.put(new Pair<>(Condition.HALL, Alphabet.HELP), new Pair<>(Condition.HALL, Answer.Help));
-        trans.put(new Pair<>(Condition.HALL, Alphabet.ROOMS), new Pair<>(Condition.ROOMS, Answer.RoomsMock));
+        trans.put(new Pair<>(Condition.HALL, Alphabet.ROOMS), new Pair<>(Condition.ROOMS, Answer.Rooms));
 
         trans.put(new Pair<>(Condition.ROOMS, Alphabet.CANCEL), new Pair<>(Condition.HALL, Answer.Intro));
         trans.put(new Pair<>(Condition.ROOMS, Alphabet.GOTO), new Pair<>(Condition.FILES, Answer.GoToMsg));
 
-        trans.put(new Pair<>(Condition.FILES, Alphabet.CANCEL), new Pair<>(Condition.HALL, Answer.Intro));
+        trans.put(new Pair<>(Condition.FILES, Alphabet.CANCEL), new Pair<>(Condition.CATS, Answer.Cats));
+        trans.put(new Pair<>(Condition.CATS, Alphabet.CANCEL), new Pair<>(Condition.ROOMS, Answer.Rooms));
 
         trans.put(new Pair<>(Condition.FILES_GET, Alphabet.CANCEL), new Pair<>(Condition.FILES, Answer.GoToMsg));
         trans.put(new Pair<>(Condition.FILES_SEND, Alphabet.CANCEL), new Pair<>(Condition.FILES, Answer.GoToMsg));
+
+        trans.put(new Pair<>(Condition.ROOMS_CREATE, Alphabet.CANCEL), new Pair<>(Condition.ROOMS, Answer.Rooms));
+        trans.put(new Pair<>(Condition.ROOMS_CHOOSE, Alphabet.CANCEL), new Pair<>(Condition.ROOMS, Answer.Rooms));
+
+        trans.put(new Pair<>(Condition.CATS_CHOOSE, Alphabet.CANCEL), new Pair<>(Condition.CATS, Answer.Cats));
+        trans.put(new Pair<>(Condition.CATS_CREATE, Alphabet.CANCEL), new Pair<>(Condition.CATS, Answer.Cats));
     }
 
     public HashMap<Pair<Condition, String>, Pair<Condition, String>> getTransitions() {
--- source/src/main/java/util/Util.java
+++ source/src/main/java/util/Util.java
@@ -1,9 +1,11 @@
 package util;
 
+import handler.Category;
 import handler.MessageFile;
+import handler.Room;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collection;
 
 public class Util {
     public static Boolean BeginWith(String in, char symbol) {
@@ -13,6 +15,22 @@ public class Util {
     public static String GetNamesOfFiles(ArrayList<MessageFile> files) {
         var result = new StringBuilder();
         files.forEach(f -> result.append(f.getName()).append(", "));
+        if (result.length() < 3)
+            return result.toString();
+        return result.substring(0, result.length() - 2);
+    }
+
+    public static String GetNamesOfRooms(ArrayList<Room> rooms) {
+        var result = new StringBuilder();
+        rooms.forEach(r -> result.append(r.getName()).append(", "));
+        if (result.length() < 3)
+            return result.toString();
+        return result.substring(0, result.length() - 2);
+    }
+
+    public static String GetNamesOfCats(Collection<Category> rooms) {
+        var result = new StringBuilder();
+        rooms.forEach(r -> result.append(r.getName()).append(", "));
         if (result.length() < 3)
             return result.toString();
         return result.substring(0, result.length() - 2);
